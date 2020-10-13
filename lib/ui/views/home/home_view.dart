819c026b8ac1d65aadde2ce9f3ebf17302a7ddd0
Hary <phdhary@gmail.com>
2020-10-13 17:13:29 +0700

    add a state changes by pressing fbtn

--- lib/ui/views/home/home_view.dart
+++ lib/ui/views/home/home_view.dart
@@ -7,9 +7,13 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ViewModelBuilder<HomeViewModel>.reactive(
-      builder: (context, model,child) => Scaffold(
+      builder: (context, model, child) => Scaffold(
         body: Center(
           child: Text(model.title),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: model.updateCounter,
         ),
       ),
       viewModelBuilder: () => HomeViewModel(),
